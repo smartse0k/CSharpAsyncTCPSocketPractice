41235cbbb97de1872a40264efc14f8de06dd57e1
smartse0k <smartse0k@naver.com>
2022-06-05 21:15:21 +0900

    EchoClient 프로젝트 추가

---
+++ EchoClient/Client.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoClient
+{
+    internal class Client
+    {
+        private Socket socket;
+
+        public Client(string host, ushort port)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(host, port);
+
+            SocketAsyncEventArgs recvEvent = new SocketAsyncEventArgs();
+            recvEvent.SetBuffer(new byte[1024], 0, 1024);
+            recvEvent.Completed += OnRecvComplete;
+            StartReceive(recvEvent);
+        }
+
+        private void StartReceive(SocketAsyncEventArgs e)
+        {
+            Console.WriteLine("StartReceive()");
+
+            socket.ReceiveAsync(e);
+        }
+
+        public int Send(byte[] data)
+        {
+            return socket.Send(data);
+        }
+
+        private void OnRecvComplete(object? sender, SocketAsyncEventArgs e)
+        {
+            Console.WriteLine("OnRecvComplete()");
+
+            if (e.BytesTransferred == 0)
+            {
+                Console.WriteLine("  연결이 끊어짐!");
+                return;
+            }
+
+            if (e.Buffer == null)
+            {
+                Console.WriteLine("  OnRecvComplete() 실패! 버퍼가 존재하지 않음.");
+                return;
+            }
+
+            Console.WriteLine("  수신된 크기: {0} 바이트", e.BytesTransferred.ToString());
+            Console.WriteLine("  수신된 정보: {0}", Encoding.Default.GetString(e.Buffer, 0, e.BytesTransferred));
+
+            StartReceive(e);
+        }
+    }
+}
---
+++ EchoClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using EchoClient;
+
+Client client = new Client("127.0.0.1", 10000);
+
+while (true)
+{
+    string? input = Console.ReadLine();
+
+    if (input == null)
+    {
+        break;
+    }
+
+    byte[] data = System.Text.Encoding.Default.GetBytes(input);
+    client.Send(data);
+}
